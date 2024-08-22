41db04466cf6b5e9f9435be7cf26faa1a0ef6161
alex-web-dev <alex.web-dev@mail.ru>
2024-08-22 20:38:58 +0300

    fix: close menu on calculate button click

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,22 +1,23 @@
 const $menu = document.querySelector('.mobile-menu');
 const delay = 600;
-let animate = false;
 
 if ($menu) {
   const $menuToggleBtns = document.querySelectorAll('.js-toggle-menu');
   $menuToggleBtns.forEach($menuToggle => {
     $menuToggle.addEventListener('click', () => {
       toggle($menu, $menuToggleBtns)
-      animate = true;
     
       if ($menu.classList.contains('mobile-menu--show')) {
         $menu.classList.remove("mobile-menu--show");
       } else {
         setTimeout(() => $menu.classList.add("mobile-menu--show"), delay);
       }
-  
-      setTimeout(() => animate = false, delay);
     });
+  });
+
+  const $closeMenuBtns = document.querySelectorAll('.js-close-menu');
+  $closeMenuBtns.forEach($closeBtn => {
+    $closeBtn.addEventListener('click', () => close($menu, $menuToggleBtns));
   });
 }
 
@@ -24,4 +25,10 @@ function toggle($menu, $menuToggleBtns) {
   $menu?.classList.toggle('mobile-menu--active');
   $menuToggleBtns.forEach($menuToggle => $menuToggle.classList.toggle('menu-toggle--active'));
   document.body.classList.toggle('body--lock')
+}
+
+function close($menu, $menuToggleBtns) {
+  $menu?.classList.remove('mobile-menu--active');
+  $menuToggleBtns.forEach($menuToggle => $menuToggle.classList.remove('menu-toggle--active'));
+  document.body.classList.remove('body--lock')
 }
